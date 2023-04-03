e6925b8f4c012ae011de4caaa2c95dd7074c1544
Negar609 <58275316+Negar609@users.noreply.github.com>
2023-04-03 14:14:31 -0400

    Update airlines.js

--- flight-search-master/app/controllers/airlines.js
+++ flight-search-master/app/controllers/airlines.js
@@ -7,3 +7,9 @@ module.exports = (app) => {
     }).catch(next);
   });
 };
+for sure 
+we could make it better 
+ at tehe end  w e we  choronic res.json(airlines)
+;
+master branch? what is it? what am i doing bro recently we coukd make it so much better 
+apps and tools working so well then we culd make.  it so muchbter 
